9ca3bde2766199b84c004ce6c30f8af75b6fc637
张新栋 <2072246648@qq.com>
2026-03-03 12:59:08 +0800

    删除菜品，以及菜品分页查询的开发

--- sky-pojo/src/main/java/com/sky/vo/DishVO.java
+++ sky-pojo/src/main/java/com/sky/vo/DishVO.java
@@ -1,5 +1,6 @@
 package com.sky.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sky.entity.DishFlavor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,6 +32,7 @@ public class DishVO implements Serializable {
     //0 停售 1 起售
     private Integer status;
     //更新时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
     //分类名称
     private String categoryName;
